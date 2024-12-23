73a178635774e1ef98a2d92a14949633206d6449
Vladuslav Hnidyi <vgnedoj03@gmail.com>
2024-12-23 02:03:50 +0200

    nodeHw04

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,52 @@
+import { SORT_ORDER } from '../constants/contacts.js';
 import { contactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  const contacts = await contactsCollection.find();
-  return contacts;
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+  const contactsQuery = contactsCollection.find();
+  if (filter.type) {
+    contactsQuery.where('contactType').equals(filter.type);
+  }
+
+  if (filter.isFavourite !== undefined) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+  // const countContacts = await contactsCollection
+  //   .find()
+  //   .merge(contactsQuery)
+  //   .countDocuments();
+
+  // const contacts = await contactsQuery
+
+  //   .limit(limit)
+  //   .skip(skip)
+  //   .sort({ [sortBy]: sortOrder })
+  //   .exec();
+
+  const [countContacts, contacts] = await Promise.all([
+    contactsCollection.find().merge(contactsQuery).countDocuments(),
+    contactsQuery
+      .find()
+      .limit(limit)
+      .skip(skip)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(countContacts, page, perPage);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
